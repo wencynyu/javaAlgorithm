5d08987f61721b6cbe14b576fcbd2b7578e4cae7
yuwenxin <yuwenxin@starblink.co>
2023-05-22 13:39:59 +0800

    add main date

--- src/main/java/top/yuwenxin/Main.java
+++ src/main/java/top/yuwenxin/Main.java
@@ -6,7 +6,7 @@ import top.yuwenxin.net.vertx.HelloWorldVerticle;
 public class Main {
 
     public static void main(String[] args) {
-        // vertx异步http处理框架
+        // vertx异步http处理框架-2023.5.22
         Vertx vertx = Vertx.vertx();
         vertx.deployVerticle(HelloWorldVerticle.class.getName());
     }
